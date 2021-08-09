b0c4b7ac9d84998788adfb0ad0d4b0232cc5a60a
Donish99 <nurakh99@gmail.com>
2021-08-09 20:06:00 +0500

    project completed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import SearchParams from "./SearchParams";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Details from "./Details";
 
+import ThemeContext from "./ThemeContext";
+
 const App = () => {
+  const theme = useState("darkblue");
   return (
-    <div>
-      <Router>
-        <header>
-          <Link to="/">
-            <h1>Adopt Me</h1>
-          </Link>
-        </header>
-        <Switch>
-          <Route path="/details/:id">
-            <Details />
-          </Route>
-          <Router path="/">
-            <SearchParams />
-          </Router>
-        </Switch>
-      </Router>
-    </div>
+    <ThemeContext.Provider value={theme}>
+      <div>
+        <Router>
+          <header>
+            <Link to="/">
+              <h1>Adopt Me</h1>
+            </Link>
+          </header>
+          <Switch>
+            <Route path="/details/:id">
+              <Details />
+            </Route>
+            <Router path="/">
+              <SearchParams />
+            </Router>
+          </Switch>
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
